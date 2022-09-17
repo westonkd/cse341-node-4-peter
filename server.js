94738fc8d564d2d3f182ab8d4235118d9bb39697
peterjwatson <peterskyguy@gmail.com>
2022-09-17 19:48:55 -0400

    main

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = 3000
 
 app.use('/', require('./routes'))
 
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
