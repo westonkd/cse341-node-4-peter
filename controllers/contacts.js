8354f3a4847404c50d5eb2ac28d5d2fa046278af
peterjwatson <peterskyguy@gmail.com>
2022-09-26 21:30:09 -0600

    contacts change

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -9,22 +9,22 @@ const getAll = async (req, res, next) => {
     });
 };
 
-const getSingle = async (req, res, next) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-        .getDb()
-        .db()
-        .collection('contacts')
-        .find({
-            _id: userId
-        });
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[0]);
-    });
-};
+// const getSingle = async (req, res, next) => {
+//     const userId = new ObjectId(req.params.id);
+//     const result = await mongodb
+//         .getDb()
+//         .db()
+//         .collection('contacts')
+//         .find({
+//             _id: userId
+//         });
+//     result.toArray().then((lists) => {
+//         res.setHeader('Content-Type', 'application/json');
+//         res.status(200).json(lists[0]);
+//     });
+// };
 
 module.exports = {
-    getAll,
-    getSingle
+    getAll
+    // getSingle
 };
