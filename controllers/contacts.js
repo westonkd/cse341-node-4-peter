7538f76e4b5512d8194692bfc906f5e3d102c60a
peterjwatson <peterskyguy@gmail.com>
2022-09-26 22:27:14 -0600

    change contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,29 +2,29 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-    const result = await mongodb.getDb().db('contacts').collection('contacts').find();
+    const result = await mongodb.getDb().db('cse-341').collection('contacts').find();
     result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(lists);
     });
 };
 
-// const getSingle = async (req, res, next) => {
-//     const userId = new ObjectId(req.params.id);
-//     const result = await mongodb
-//         .getDb()
-//         .db()
-//         .collection('contacts')
-//         .find({
-//             _id: userId
-//         });
-//     result.toArray().then((lists) => {
-//         res.setHeader('Content-Type', 'application/json');
-//         res.status(200).json(lists[0]);
-//     });
-// };
+const getSingle = async (req, res, next) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+        .getDb('contacts')
+        .db()
+        .collection('contacts')
+        .find({
+            _id: userId
+        });
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists[0]);
+    });
+};
 
 module.exports = {
-    getAll
-    // getSingle
+    getAll,
+    getSingle
 };
